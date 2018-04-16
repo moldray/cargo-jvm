f1231706b8d16a3d4a6103628be9d4b4cdcc083c
雷东升 <leidongsheng@luojilab.com>
2018-04-16 17:04:29 +0800

    save

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+extern crate clap; 
+use clap::App; 
+
+fn main() { 
+  App::new("cargo-jvm")
+    .version("1.0.0")
+    .about("jilia version manager with rust cargo")
+    .author("moldray<leids@foxmail.com>")
+    .get_matches(); 
 }
